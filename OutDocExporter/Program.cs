b43e6ca617671327b65fa47bee16d79d70a87e56
Junji Watanabe <jyunji.watanabe@house-soft.biz>
2020-07-24 18:34:35 +0900

    SS出力まで完了

--- OutDocExporter/Program.cs
+++ OutDocExporter/Program.cs
@@ -1,49 +1,60 @@
 ﻿using System;
-using OpenQA.Selenium.Chrome;
+using System.IO;
 using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Interactions;
 using Microsoft.Extensions.Configuration;
 
-namespace OutSystemsUtilities
+class Program
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        if (args.Length == 0)
         {
-            IConfiguration configuration = new ConfigurationBuilder()
-                                                .AddJsonFile("appsettings.json", true, true)
-                                                .Build();
-            var url = configuration.GetSection("HostAddress").Value + "OutDoc";
-            var userName = configuration.GetSection("UserName").Value;
-            var password = configuration.GetSection("Password").Value;
-            var eSpaceName = "HousesoftSampleTraditional";
-            try
-            {            
-                DownloadDocument(url, userName, password, eSpaceName);
-            }
-            catch (System.Exception ex)
-            {
-                System.Console.WriteLine(ex.Message + Environment.NewLine + ex.StackTrace);
-            }
+            System.Console.WriteLine("ドキュメントを出力するモジュール名を指定してください。");
+            return;
         }
-
-        private static void DownloadDocument(string url, string userName, string password, string eSpaceName)
+        var eSpaceName = args[0];
+        if (Directory.Exists(ScreenShot.Path) == false)
         {
-            using (var driver = new ChromeDriver())
-            {
-                // ログインページヘ遷移し、ログインを実行する
-                driver.Navigate().GoToUrl(url);
-                var loginPage = new Login(driver);
-                loginPage.SetCredential(userName, password);
-                var homeScreen = loginPage.LoginAndGoToHomeScreen();
-                // eSpacesページへ遷移する
-                var eSpacesList = homeScreen.MoveToESpaceList();
-                // eSpace名で検索して対象モジュールのみ表示した上で、クリックして開く（ドキュメントページへ）
-                var eSpaceDesingFeedBack = eSpacesList.OpenESpace(eSpaceName);
-                // 生成されたドキュメントを開く
-                eSpaceDesingFeedBack.WaitForButtonAndClick();
+            System.Console.WriteLine($"出力先フォルダ{ScreenShot.Path}を作成するか、出力先フォルダを変更してください");
+            return;
+        }
+        IConfiguration configuration = new ConfigurationBuilder()
+                                            .AddJsonFile("appsettings.json", true, true)
+                                            .Build();
+        var url = configuration.GetSection("HostAddress").Value + "OutDoc";
+        var userName = configuration.GetSection("UserName").Value;
+        var password = configuration.GetSection("Password").Value;
+        try
+        {            
+            DownloadDocument(url, userName, password, eSpaceName);
+        }
+        catch (System.Exception ex)
+        {
+            System.Console.WriteLine(ex.Message + Environment.NewLine + ex.StackTrace);
+        }
+    }
 
-                var debugdummy = "dummy";   // デバッグ用（ブラウザ表示した状態でブレークするため）ダミー行
-            }
+    private static void DownloadDocument(string url, string userName, string password, string eSpaceName)
+    {
+        using (var driver = new ChromeDriver())
+        {
+            // ログインページヘ遷移し、ログインを実行する
+            driver.Navigate().GoToUrl(url);
+            var loginPage = new Login(driver);
+            loginPage.SetCredential(userName, password);
+            var homeScreen = loginPage.LoginAndGoToHomeScreen();
+            // eSpacesページへ遷移する
+            var eSpacesList = homeScreen.MoveToESpaceList();
+            // eSpace名で検索して対象モジュールのみ表示した上で、クリックして開く（ドキュメントページへ）
+            var eSpaceDesingFeedBack = eSpacesList.OpenESpace(eSpaceName);
+            // 生成されたドキュメントを開く
+            eSpaceDesingFeedBack.WaitForButtonAndClick();
+            // スクリーンショットを取得
+            ScreenShot.TakeWholePageAsScreenShot(driver);
+
+            var debugdummy = "dummy";   // デバッグ用（ブラウザ表示した状態でブレークするため）ダミー行
         }
     }
 }
--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # OutSystemsUtilities
 utility programs for OutSystems development or management
+
+## OutDocExporter
+Using Selenium ChromeDriver, save the OutDoc document of specified module as screenshots.
+To be fixed: Combine screenshots to one file.
+
+### Prerequisite
+Place an appsettings.json file at the same place of your exe file.
+Edit it to have your OutSystems environment information there.
+The screen shots will be outputted in C:\\work\ss folder, so ensure that the folder exists or change the path.
+
+### Command
+Open up your console and type as follows:
+OutSystemsUtilities.exe <your_module_name>
